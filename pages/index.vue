a6ee1b9be8e300a4b9966a8eaf19dc59a98754c5
niranjan-talkinglands <niranjan@talkinglands.com>
2025-01-03 17:31:50 +0530

    test

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,11 @@
 <script setup lang="ts">
 import { APP_NAME } from '~/utils/constants';
 
+onMounted(() => {
+  //@ts-ignore
+  window.ReactNativeWebView.postMessage('Message from web', '*');
+});
+
 definePageMeta({
   layout: 'app'
 });
